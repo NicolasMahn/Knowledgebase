ee7b17ba9844688ce95ba4c262f50ee6b5e5aa1c
LeonRottler <l.rottler@protonmail.com>
2024-06-03 14:44:05 +0200

    integrated the chat client with the query_data function

--- chat_client.py
+++ chat_client.py
@@ -1,9 +1,15 @@
 import gradio as gr
 
+import query_data
+
 def chat_function(message: str, history: list):
     # hier der aufruf zu Nicolas seinem Code
+    topic_dir = "data/franka_emika_panda"
+    chroma_dir = f"{topic_dir}/chroma"
+    data_dir = f"{topic_dir}/documents"
+    response = query_data.query_rag(message, data_dir, chroma_dir)
 
-    return "Test response"
+    return response
 
 demo = gr.ChatInterface(
     fn=chat_function,
